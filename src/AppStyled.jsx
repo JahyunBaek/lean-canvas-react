32aa7b06447612611b6a04acb00bffc9f07b0550
JahyunBaek <backjahyun@naver.com>
2026-01-08 23:09:02 +0900

    [ADD] tailwind CSS

--- src/AppStyled.jsx
+++ src/AppStyled.jsx
@@ -1,9 +1,12 @@
 import Card from './components/StyledComponents/Card';
 import BaseButton from './components/StyledComponents/BaseButton';
 import StyledButtobn from './components/StyledComponents/StyledButton';
+import './App.css';
+
 const AppStyled = () => {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Card />
       <br />
       <BaseButton>BaseButton</BaseButton>
